f4bc3cda0a22c0fb5aafa12ddc97d4172689984d
nakamu <oyukita56@gmail.com>
2023-06-11 13:20:08 +0900

    データをv-forで出すよう修正

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,27 +1,44 @@
-<script setup>
+<script>
 import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
+export default {
+  components: {
+    WelcomeItem
+   }
+   ,
+  data() {
+    return {
+      unagero: {
+        1: {
+          date: "2021年7月10日",
+          name: "#1　ショウタとカネ",
+          info: "podcastスタート/うなげろりん命名理由/写真集の構想"
+        },
+        2: {
+          date: "2021年7月24日",
+          name: "#2　小僧解体新書",
+          info: "中谷さんの黒歴史ブログ公開"
+        },
+        3: {
+          date: "2021年8月7日",
+          name: "#3　救世主登場",
+          info: "写真集の救世主"
+        }
+      }
+    }
+  }
+}
 </script>
 
 <template>
-  <WelcomeItem>
-    <template #icon>
-      21.7.10
-    </template>
-    <template #heading>#1 ショウタとカネ</template>
-    - podcastスタート
-    - うなげろりん命名理由
-    - 写真集の構想
-  </WelcomeItem>
-  <WelcomeItem>
-    <template #icon>
-      21.7.24
-    </template>
-    <template #heading>#2 小僧解体新書</template>
-    - 中谷さんの黒歴史ブログ公開
-  </WelcomeItem>
+
+  <div v-for="(item, key) in unagero" :key="key">
+      <WelcomeItem>
+        <template #icon>
+          {{ item.date }}
+        </template>
+        <template #heading>{{ item.name }}</template>
+        {{ item.info }}
+      </WelcomeItem>
+  </div>
+
 </template>
