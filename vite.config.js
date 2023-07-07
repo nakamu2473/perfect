5df7a7f74da7be04739c07bc2bbcd59d270dcc11
nakamu <oyukita56@gmail.com>
2023-07-07 22:03:30 +0900

    GitHub Pages用の設定を消す

--- vite.config.js
+++ vite.config.js
@@ -11,8 +11,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: "/perfect/",
-  build: {
-    outDir: "docs"
-  },
+  // base: "/perfect/",
+  // build: {
+  //   outDir: "docs"
+  // },
 })
