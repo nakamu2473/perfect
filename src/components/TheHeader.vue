22e2a7f19ec71b4f70b63d02422eac7285aba7cb
nakamu <oyukita56@gmail.com>
2023-07-12 08:26:33 +0900

    ヘッダーのリンク位置調整

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,6 +1,14 @@
 <template>
-    <div>
+    <nav>
         <router-link to="/">Home</router-link> /
         <router-link to="/linkList">linkList</router-link> 
-    </div>
+    </nav>
 </template>
+<style>
+nav {
+    width: 100%;
+    font-size: 14px;
+    text-align: center;
+    margin-top: 2rem;
+  }
+</style>
