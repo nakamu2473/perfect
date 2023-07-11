15e1d7cfd8ff20c2740829a425976335f2865929
nakamu <oyukita56@gmail.com>
2023-07-12 08:26:54 +0900

    流行語大賞のデータが表示できるよう修正

---
+++ src/components/RankItem.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="item">
+      <i>
+        <slot name="icon"></slot>
+      </i>
+      <div class="details">
+        <h3>
+          <slot name="title"></slot>
+        </h3>
+        <slot name="heading"></slot>
+      </div>
+    </div>
+  </template>
+  
+  <style scoped>
+  
+  .details {
+    flex: 1;
+    /* margin-left: 1rem; */
+  }
+  .item {
+    position: relative;
+    margin-top: 1;
+    margin-bottom: 1;
+    padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
+}
+  
+  h3 {
+    font-size: 1.2rem;
+    font-weight: 500;
+    margin-bottom: 0.4rem;
+    color: var(--color-heading);
+  }
+  
+  i {
+      /* top: calc(50% - 25px); */
+      left: 0px;
+      position: absolute;
+      /* border: 1px solid var(--color-border); */
+      background: var(--color-background);
+      /* border-radius: 8px; */
+      /* width: 10px; */
+      /* height: 10px; */
+  }
+  
+  
+  
+  </style>
--- src/pages/history.vue
+++ src/pages/history.vue
@@ -1,16 +1,20 @@
 <script>
 import ListItem from '../components/ListItem.vue'
+import RankItem from '../components/RankItem.vue'
 import { history } from "../data/history"
+import { rank2022 } from "../data/rank2022"
 
 
 export default {
   components: {
-    ListItem
+    ListItem,
+    RankItem
    }
    ,
   data() {
     return {
       history: history,
+      rank2022: rank2022,
     }
   }
 }
@@ -20,56 +24,25 @@ export default {
 
 <template> 
   <div class="rank" > 
-    <div class="rank_title" style="background-color: red; height: 50px;" > 
-
+    <div class="rank_title" style="background-color: rgb(254, 236, 236); height: 50px;" > 
       <p>流行語大賞2022</p>
     </div>
-    <div class="body" style="padding-left: 50px; padding-right: 50px;">
-      <div style="background-color: yellow; border: solid black 1px; display:flex;">
-        <div  style="position:relative;">
-          <img src="../icons/crown.png" style=" width: 60px;" /><span style="position: absolute;top:16px;left:22px;"> 1位</span></div>
-        <div>
-          <div style="">せつめい！</div>
-          <div>せつめい！</div>
+
+    <RankItem msg="Hello" v-for="(item, key) in rank2022" :key="key">
+        <!-- <div class="body" style="padding-left: 50px; padding-right: 50px;"></div> -->
+        <template #icon >
+          <img src="../icons/crown.png" style=" width: 60px; position:absolute;" />
+          <span style="position:absolute;top:16px;left:28px;font-family:monospace;">{{ item.no }}</span>
+        </template>
+        <template #heading>
+          <div style=" display:flex;">
+            <div>
+              <div style="">{{ item.podcasttitle }}</div>
+              <a v-if=" item.title" :href="item.url"   target="_blank" rel="noopener noreferrer">{{ item.title }}</a><br v-if=" item.title">
+          </div>
         </div>
-      </div>
-      <div style="background-color: gray; height: 60px; border: solid black 1px; display:flex;">
-        <div  style="position:relative;">
-          <img src="../icons/crown.png" style=" width: 60px;" /><span style="position: absolute;top:16px;left:22px;"> 2位</span></div>
-        <div>
-          <div style="">せつめい！</div>
-          <div>せつめい！</div>
-        </div>
-      </div>
-      <div style="background-color: orange; height: 60px; border: solid black 1px;display:flex;">
-        <div  style="position:relative;">
-          <img src="../icons/crown.png" style=" width: 60px;" /><span style="position: absolute;top:16px;left:22px;"> 3位</span></div>
-        <div>
-          <div style="">せつめい！</div>
-          <div>せつめい！</div>
-        </div>
-      </div>
-      <div style="background-color: pink; height: 60px; border: solid black 1px;display:flex;">
-        <div  style="position:relative;">
-          <img src="../icons/crown.png" style=" width: 60px;" /><span style="position: absolute;top:16px;left:22px;"> 4位</span></div>
-        <div>
-          <div style="">せつめい！</div>
-          <div>せつめい！</div>
-        </div>
-      </div>
-      <div style="background-color: skyblue; height: 60px; border: solid black 1px;display:flex;">
-        <div  style="position:relative;">
-          <img src="../icons/crown.png" style=" width: 60px;" /><span style="position: absolute;top:16px;left:22px;"> 5位</span></div>
-        <div>
-          <div style="">せつめい！</div>
-          <div>せつめい！</div>
-        </div>
-      </div>
-      １位から ５位までならべるぞー
-      deteru!hai!
-      カラフル！
-    </div>
-    
+        </template>
+    </RankItem>
   </div>
   <div class="history" > 
       <p>二人の歴史</p>
