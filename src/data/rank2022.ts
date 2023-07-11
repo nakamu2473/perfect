a4efecbf21ee15c432d4a8e78b7afe9875036f59
FruitRiin <nassii74@gmail.com>
2023-07-11 21:49:04 +0900

    流行語
    Co-authored-by: nakamu2473 <nakamu2473@users.noreply.github.com>
    Co-authored-by: Renoa.ts <OKAUEND@users.noreply.github.com>
    Co-authored-by: tns <tnssn@users.noreply.github.com>

---
+++ src/data/rank2022.ts
@@ -0,0 +1,62 @@
+const rank2022 = [
+    {
+      "no": 1,
+      "title": "ドラえもんぶるな",
+      "podcast-title": "#33 ルーレット",
+      "url": "https://open.spotify.com/episode/4oKFMXlfefGp2f9FPbZyXX?si=SVVOvlj1R8yEuHlrTllKzg&t=499&context=spotify%3Aepisode%3A4oKFMXlfefGp2f9FPbZyXX"
+    },
+    {
+      "no": 2,
+      "title": "肉棒餃子",
+      "podcast-title": "#44 中谷裕太、手術します",
+      "url": "https://open.spotify.com/episode/3NAPkKLX01vZIVYoR5C3Nb?si=qIfeJWbWSJSmZ8KZQNTjBQ&context=spotify%3Ashow%3A223Rj9cjUGjcCY1CUP4ahP&t=1384"
+    },
+    {
+      "no": 3,
+      "title": "S○SEX",
+      "podcast-title": "#51 S○S○X",
+      "url": "https://open.spotify.com/episode/2KyybAS7ntKvyf3Si0KRY7?si=QRPOUKWHQGKyn6lkZ0BoAw&t=1059&context=spotify%3Ashow%3A223Rj9cjUGjcCY1CUP4ahP"
+    },
+    {
+      "no": 4,
+      "title": "というのも",
+      "podcast-title": "#48 というのも",
+      "url": "https://open.spotify.com/episode/1HkEGAQsgvsiy1fy1B7HkG?si=SkswvIv7QDOTKcHRHuk8iQ&context=spotify%3Ashow%3A223Rj9cjUGjcCY1CUP4ahP&t=1522"
+    },
+    {
+      "no": 5,
+      "title": "ワオ、くさっ！",
+      "podcast-title": "#21 シモテロリスト",
+      "url": "https://open.spotify.com/episode/1Qqac5G84pDSrOpVAWNusJ?si=sUnnc4CGR3GXoOO2tiDW7Q&t=1675&context=spotify%3Ashow%3A223Rj9cjUGjcCY1CUP4ahP"
+    },
+    {
+      "no": 6,
+      "title": "ウザいから年とらんといてほしい",
+      "podcast-title": "#60 考えてくれてる？俺の誕生日プレゼント",
+      "url": ""
+    },
+    {
+      "no": 7,
+      "title": "バカ臭い",
+      "podcast-title": "#36 キモ萌えキュン",
+      "url": "https://open.spotify.com/episode/5G2SvPOnwVr06Iz5WPZH4M?si=lIxmiXjLT3uzTt04ehsyVA&t=595&context=spotify%3Ashow%3A223Rj9cjUGjcCY1CUP4ahP"
+    },
+    {
+      "no": 8,
+      "title": "休日の過ごし方：インドア＆アウトドア",
+      "podcast-title": "#58 空白の2時間",
+      "url": "https://open.spotify.com/episode/4g2FP0iAfGE8ijPStdMgkK?si=-rZorT21QKOIGNdL0xbRrw&t=1617&context=spotify%3Ashow%3A223Rj9cjUGjcCY1CUP4ahP"
+    },
+    {
+      "no": 9,
+      "title": "ニチッ",
+      "podcast-title": "",
+      "url": ""
+    },
+    {
+      "no": 10,
+      "title": "洋モノの老婆",
+      "podcast-title": "#57 キレすぎんといてよ～",
+      "url": "https://open.spotify.com/episode/3IMnUJcTlzRcdO2eWEKU0n?si=jDcyTpELTB-FUBc0eB1BXg&t=1575&context=spotify%3Ashow%3A223Rj9cjUGjcCY1CUP4ahP"
+    }
+  ]
